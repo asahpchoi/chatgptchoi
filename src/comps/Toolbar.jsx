9130e6fa3d8cf45859e0cd73e428fc5f10432a7f
Asa Choi <asa.choi@gmail.com>
2023-04-08 04:55:48 +0000

    X

--- src/comps/Toolbar.jsx
+++ src/comps/Toolbar.jsx
@@ -5,17 +5,29 @@ import { Button } from "@mui/material";
 import "animate.css";
 
 export function Toolbar({ addMessage }) {
+  const send = () => {
+    const msg = document.getElementById("message").value;
+    document.getElementById("message").value = "";
+    addMessage(msg);
+  };
   return (
-    <TextField
-      id="message"
-      style={{ width: "90%" }}
-      onKeyPress={(event) => {
-        if (event.key === "Enter") {
-          const msg = document.getElementById("message").value;
-          document.getElementById("message").value = "";
-          addMessage(msg);
-        }
-      }}
-    />
+    <>
+      <TextField
+        id="message"
+        style={{ width: "90%" }}
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            send();
+          }
+        }}
+      />
+      <Button
+        onClick={() => {
+          send();
+        }}
+      >
+        Send
+      </Button>
+    </>
   );
 }
